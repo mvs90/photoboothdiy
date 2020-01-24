aa5837607893b776dee5395bb2c83badc1147fa3
Marc Schremmer <marc.schremmer@icloud.com>
2020-01-24 01:25:03 +0100

    Update camera.py

--- camera.py
+++ camera.py
@@ -81,7 +81,7 @@ def input(events):
 			
 # set variables to properly display the image on screen at right ratio
 def set_demensions(img_w, img_h):
-	# Note this only works when in booting in desktop mode. 
+    # Note this only works when in booting in desktop mode.
 	# When running in terminal, the size is not correct (it displays small). Why?
 
     # connect to global vars
@@ -210,9 +210,9 @@ def UpdateDisplay():
                     background.blit(text, textpos)
     
     if(ImageShowed == True):
-    	screenPicture.blit(backgroundPicture, (0, 0))   	
+        screenPicture.blit(backgroundPicture, (0, 0))
     else:
-    	screen.blit(background, (0, 0))
+        screen.blit(background, (0, 0))
    
     pygame.display.flip()
     return
@@ -234,51 +234,49 @@ def ShowPicture(file, delay):
 	
 # display one image on screen
 def show_image(image_path):	
-	screen.fill(pygame.Color("white")) # clear the screen	
-	img = pygame.image.load(image_path) # load the image
-	img = img.convert()	
-	set_demensions(img.get_width(), img.get_height()) # set pixel dimensions based on image	
-	x = (infoObject.current_w / 2) - (img.get_width() / 2)
-	y = (infoObject.current_h / 2) - (img.get_height() / 2)
-	screen.blit(img,(x,y))
-	pygame.display.flip()
+    screen.fill(pygame.Color("white")) # clear the screen
+    img = pygame.image.load(image_path) # load the image
+    img = img.convert()
+    set_demensions(img.get_width(), img.get_height()) # set pixel dimensions based on image
+    x = (infoObject.current_w / 2) - (img.get_width() / 2)
+    y = (infoObject.current_h / 2) - (img.get_height() / 2)
+    screen.blit(img,(x,y))
+    pygame.display.flip()
 
 def CapturePicture():
-        global imagecounter
-        global imagefolder
-        global Numeral
-        global Message
-        global screen
-        global background
-        global screenPicture
-        global backgroundPicture
-        global pygame
-        global ImageShowed
-        global CountDownPhoto
-	global BackgroundColor
-	
-	BackgroundColor = ""
-	Numeral = ""
-        Message = ""
-	UpdateDisplay()
-	time.sleep(1)
-	CountDownPhoto = ""
-	UpdateDisplay()
-	background.fill(pygame.Color("black"))
-	screen.blit(background, (0, 0))
-	pygame.display.flip()
-	camera.start_preview()
-	BackgroundColor = "black"
-	for x in range(5, -1, -1):
-                if x == 0:                        
-                        Numeral = ""
-                        Message = "LOS GEHT'S..."
-                else:                        
-                        Numeral = str(x)
-                        Message = ""                
-                UpdateDisplay()
-                time.sleep(1)
-
+    global imagecounter
+    global imagefolder
+    global Numeral
+    global Message
+    global screen
+    global background
+    global screenPicture
+    global backgroundPicture
+    global pygame
+    global ImageShowed
+    global CountDownPhoto
+    global BackgroundColor
+    BackgroundColor = ""
+    Numeral = ""
+    Message = ""
+    UpdateDisplay()
+    time.sleep(1)
+    CountDownPhoto = ""
+    UpdateDisplay()
+    background.fill(pygame.Color("black"))
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
+    camera.start_preview()
+    BackgroundColor = "black"
+    for x in range(5, -1, -1):
+        if x == 0:
+            Numeral = ""
+            Message = "LOS GEHT'S..."
+        else:
+            Numeral = str(x)
+            Message = ""
+            UpdateDisplay()
+            time.sleep(1)
         BackgroundColor = ""
         Numeral = ""
         Message = ""
@@ -288,83 +286,78 @@ def CapturePicture():
         filename = os.path.join(imagefolder, 'images', str(imagecounter)+"_"+str(ts) + '.jpg')
         camera.capture(filename)
         camera.stop_preview()
-        #ShowPicture(filename, 2)
         ImageShowed = False
         return filename
-    
-	
+
 def TakePictures():
-        global imagecounter
-        global imagefolder
-        global Numeral
-        global Message
-        global screen
-        global background
-        global pygame
-        global ImageShowed
-        global CountDownPhoto
-	global BackgroundColor
-	global Printing
-	global PhotosPerCart
-	global TotalImageCount
-
-        input(pygame.event.get())
-        CountDownPhoto = ""        
-        filename1 = CapturePicture()
-
-        CountDownPhoto = ""
-        Message = "BITTE WARTEN..."
-        UpdateDisplay()
-
-        image1 = PIL.Image.open(filename1)
-        TotalImageCount = TotalImageCount + 1
-        # Create the final filename
-        ts = time.time()
-        Final_Image_Name = os.path.join(imagefolder, "Final_" + str(TotalImageCount)+"_"+str(ts) + ".jpg")
-        # Save it to the usb drive
-        image1.save(Final_Image_Name)
-        # Save a temp file, its faster to print from the pi than usb
-        image1.save('/home/pi/Desktop/tempprint.jpg')
-        ShowPicture('/home/pi/Desktop/tempprint.jpg',3)
-        ImageShowed = False
-        Message = "KNOPF DRUECKEN UM BILD ZU DRUCKEN"
-        UpdateDisplay()
-        time.sleep(3)
-        Message = ""
-        UpdateDisplay()
-        Printing = False
-        WaitForPrintingEvent()
-        Numeral = ""
-	Message = ""
-	print(Printing)
-        if Printing:
-                if (TotalImageCount <= PhotosPerCart):
-                        if os.path.isfile('/home/pi/Desktop/tempprint.jpg'):
-                                # Open a connection to cups
-                                conn = cups.Connection()
-                                # get a list of printers
-                                printers = conn.getPrinters()
-                                # select printer 0
-                                printer_name = printers.keys()[0]
-                                Message = "Impression en cours..."
-                                UpdateDisplay()
-                                time.sleep(1)
-                                # print the buffer file
-                                printqueuelength = len(conn.getJobs())
-                                if printqueuelength > 1:
-                                        ShowPicture('/home/pi/Desktop/tempprint.jpg',3)
-                                        conn.enablePrinter(printer_name)
-                                        Message = "Impression impossible"                
-                                        UpdateDisplay()
-                                        time.sleep(1)
-                                else:
-                                        conn.printFile(printer_name, '/home/pi/Desktop/tempprint.jpg', "PhotoBooth", {})
-                                        time.sleep(40)            
-                else:
-                        Message = "Nous vous enverrons vos photos"
-                        Numeral = ""
-                        UpdateDisplay()
-                        time.sleep(1)
+    global imagecounter
+    global imagefolder
+    global Numeral
+    global Message
+    global screen
+    global background
+    global pygame
+    global ImageShowed
+    global CountDownPhoto
+    global BackgroundColor
+    global Printing
+    global PhotosPerCart
+    global TotalImageCount
+    input(pygame.event.get())
+    CountDownPhoto = ""
+    filename1 = CapturePicture()
+    CountDownPhoto = ""
+    Message = "BITTE WARTEN..."
+    UpdateDisplay()
+    image1 = PIL.Image.open(filename1)
+    TotalImageCount = TotalImageCount + 1
+    # Create the final filename
+    ts = time.time()
+    Final_Image_Name = os.path.join(imagefolder, "Final_" + str(TotalImageCount)+"_"+str(ts) + ".jpg")
+    # Save it to the usb drive
+    image1.save(Final_Image_Name)
+    # Save a temp file, its faster to print from the pi than usb
+    image1.save('/home/pi/Desktop/tempprint.jpg')
+    ShowPicture('/home/pi/Desktop/tempprint.jpg',3)
+    ImageShowed = False
+    Message = "KNOPF DRUECKEN UM BILD ZU DRUCKEN"
+    UpdateDisplay()
+    time.sleep(3)
+    Message = ""
+    UpdateDisplay()
+    Printing = False
+    WaitForPrintingEvent()
+    Numeral = ""
+    Message = ""
+    print(Printing)
+    if Printing:
+        if (TotalImageCount <= PhotosPerCart):
+            if os.path.isfile('/home/pi/Desktop/tempprint.jpg'):
+                # Open a connection to cups
+                conn = cups.Connection()
+                # get a list of printers
+                printers = conn.getPrinters()
+                # select printer 0
+                printer_name = printers.keys()[0]
+                Message = "Impression en cours..."
+                UpdateDisplay()
+                time.sleep(1)
+                # print the buffer file
+                printqueuelength = len(conn.getJobs())
+                if printqueuelength > 1:
+                    ShowPicture('/home/pi/Desktop/tempprint.jpg',3)
+                    conn.enablePrinter(printer_name)
+                    Message = "Impression impossible"
+                    UpdateDisplay()
+                    time.sleep(1)
+            else:
+                conn.printFile(printer_name, '/home/pi/Desktop/tempprint.jpg', "PhotoBooth", {})
+                time.sleep(40)
+        else:
+            Message = "Nous vous enverrons vos photos"
+            Numeral = ""
+            UpdateDisplay()
+            time.sleep(1)
                 
         Message = ""
         Numeral = ""
@@ -410,28 +403,27 @@ def WaitForEvent():
     global pygame
     NotEvent = True
     while NotEvent:
-            input_state = GPIO.input(BUTTON_PIN)
-            if input_state == False:
-                    NotEvent = False			
+        input_state = GPIO.input(BUTTON_PIN)
+        if input_state == False:
+            NotEvent = False
+            return
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                if event.key == pygame.K_DOWN:
+                    NotEvent = False
                     return
-            for event in pygame.event.get():			
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_ESCAPE:
-                            pygame.quit()
-                        if event.key == pygame.K_DOWN:
-                            NotEvent = False
-                            return
-            time.sleep(0.2)
+            time.sleep(0.1)
 
 def main(threadName, *args):
     InitFolder()
     while True:
-            show_image('images/start_camera.jpg')
-            WaitForEvent()
-            time.sleep(0.2)
-            TakePictures()
-    GPIO.cleanup()
-
+        show_image('images/start_camera.jpg')
+        WaitForEvent()
+        time.sleep(0.2)
+        TakePictures()
+        GPIO.cleanup()
 
 # launch the main thread
 Thread(target=main, args=('Main', 1)).start()
